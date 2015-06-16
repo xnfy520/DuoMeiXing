3b32d534657b2bdf2419415f5218d7ca751bc8d4
雪念飞叶 <xnfy520@qq.com>
2015-06-16 22:07:46 +0800

    Initial Commit

---
+++ DuoMeiXing/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DuoMeiXing
+//
+//  Created by 天陨 on 15/6/16.
+//  Copyright (c) 2015年 wake. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
